bdf7046d6001e2fe70590401c8c6dd37661d8f60
Tushar Mahandyan <mahandyantushar@gmail.com>
2025-12-16 11:58:30 +0530

    removed default react and vite logo imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Products from './components/Products'
 import Header from './components/Header'
